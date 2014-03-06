91c67d7e0e78defade30437ff9b91a8ebb6fb7dc
Tommy Chan <tommytcchan@gmail.com>
2014-03-06 14:43:20 -0800

    update parser to include another case.

--- spec/panda_mask_spec.rb
+++ spec/panda_mask_spec.rb
@@ -58,7 +58,12 @@ describe "PandaMask tests with e-mail" do
 	end
 
 	it "catches emails with parenthesis variation 3" do
-		"contact us at tommytcchan at asdf dot com now.".panda_mask.should == 'contact us at [removed as it violates our terms of service] now.'
+		"contact us tommytcchan at asdf dot com now.".panda_mask.should == 'contact us [removed as it violates our terms of service] now.'
+	end
+
+	it "catches emails with text email and ." do
+		"contact us at tommytcchan at asdf.com now.".panda_mask.should == 'contact us at [removed as it violates our terms of service] now.'
+
 	end
 end
 
@@ -71,47 +76,47 @@ describe "PandaMask tests with phone #" do
 		"contact us at 415-123-3333 now.".panda_mask.should == 'contact us at [removed as it violates our terms of service] now.'
 	end
 
-	it "basic phone strip 2" do
+	it "basic phone strip 3" do
 		"contact us at 415_123_3333 now.".panda_mask.should == 'contact us at [removed as it violates our terms of service] now.'
 	end
 
-	it "basic phone strip 3" do
+	it "basic phone strip with intl code" do
 		"contact us at 1-415-123-3333 now.".panda_mask.should == 'contact us at [removed as it violates our terms of service] now.'
 	end
 
-	it "basic phone strip 4" do
+	it "basic phone strip with intl code and plus" do
 		"contact us at +1-415-123-3333 now.".panda_mask.should == 'contact us at [removed as it violates our terms of service] now.'
 	end
 
-	it "basic phone strip 2" do
+	it "basic phone strip with itnl code and plus 2" do
 		"contact us at +415-123-3333 now.".panda_mask.should == 'contact us at [removed as it violates our terms of service] now.'
 	end
 
-	it "basic phone strip 2" do
+	it "basic phone strip with +" do
 		"contact us at +4151233333 now.".panda_mask.should == 'contact us at [removed as it violates our terms of service] now.'
 	end
 
-	it "basic phone strip 2" do
+	it "basic phone strip with spaces" do
 		"contact us at 415 123 3333 now.".panda_mask.should == 'contact us at [removed as it violates our terms of service] now.'
 	end
 
-	it "basic phone strip 2" do
-		"contact us at 415.123.3333 now.".panda_mask.should == 'contact us at [removed as it violates our terms of service] now.'
+	it "basic phone strip with dots" do
+		"contact us at 415.123.3333 now.".panda_mask.should == 'contact [removed as it violates our terms of service] now.'
 	end
 
-	it "basic phone strip 2" do
+	it "basic phone strip intl and dashes" do
 		"contact us at 1-41O-123-3333 now.".panda_mask.should == 'contact us at [removed as it violates our terms of service] now.'
 	end
 
-	it "basic phone strip 2" do
+	it "basic phone strip with 1800 num and text" do
 		"contact us at 1-800-GOTRICE now.".panda_mask.should == 'contact us at [removed as it violates our terms of service] now.'
 	end
 
-	it "basic phone strip 2" do
+	it "basic phone strip with 1800 num and text 2" do
 		"contact us at +1-800-GOTRICE now.".panda_mask.should == 'contact us at [removed as it violates our terms of service] now.'
 	end
 
-	it "basic phone strip 2" do
+	it "basic phone strip only 7 charcs" do
 		"contact us at 123-4567 now.".panda_mask.should == 'contact us at [removed as it violates our terms of service] now.'
 	end
 
